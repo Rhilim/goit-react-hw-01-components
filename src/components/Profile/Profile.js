4055877d8dcaf8ee1395e9cf94d6b0fed8e24f00
Rhilim <arhilim@gmail.com>
2023-08-02 17:25:47 -0500

    task1 done

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,11 @@
-import { Container, List , ListItem} from './Profile.styled';
+import { Container, List , ListItem, Image} from './Profile.styled';
 
 
 export const Profile = ({ username, tag, location, avatar, stats}) => {
   return (
     <Container>
       <div className="description">
-        <img
+        <Image
           src={avatar}
           alt={username}
           className="avatar"
@@ -17,15 +17,15 @@ export const Profile = ({ username, tag, location, avatar, stats}) => {
       <List>
         <ListItem>
           <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
+          <span className="quantity"><b>{stats.followers}</b></span>
         </ListItem>
         <ListItem>
           <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
+          <span className="quantity"><b>{stats.views}</b></span>
         </ListItem>
         <ListItem>
           <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
+          <span className="quantity"><b>{stats.likes}</b></span>
         </ListItem>
       </List>
     </Container>
--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -4,6 +4,8 @@ export const Container = styled.div`
 text-align: center;
 margin: 0;
 padding: 50px;
+border: grey 2px solid;
+width: 240px;
 `;
 
 export const List = styled.ul`
@@ -13,15 +15,23 @@ justify-content: center;
 align-items: center;
 margin: 0;
 padding: 0;
-gap: 10px;
+
 `;
 
 export const ListItem = styled.li`
+padding: 8px;
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-border: grey 2px solid;
+outline: grey 1px solid;
 width: 80px;
 height: 80px;
+background-color: lightgrey;
+`;
+
+export const Image = styled.img`
+
+width: 100px;
+height: 100px;
 `;
